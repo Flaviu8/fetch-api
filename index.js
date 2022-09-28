69ad00ce55eb4e82193142e1538fb178b6efdd32
Flaviu8 <flavius_flaviu93@yahoo.com>
2022-09-28 19:28:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then((data) => data.json())
+.then((json) => console.log(json));
+
+
+let container = document.getElementById("container-text");
+
+async function fetchingData () {
+
+  let url = 'https://jsonplaceholder.typicode.com/posts'
+  try {
+      let res = await fetch(url);
+      return await res.json();
+  } catch (error) {
+      console.log(error);
+  }
+
+}
+
+
+async function renderListItems()  {
+
+  let data = await fetchingData()
+
+  let html = ''
+
+    data.forEach((blocksText) => {
+    
+       let htmlText = `<div class="blocksText">
+                            <h2>${blocksText.id}</h2>
+                            <h1 class="title">${blocksText.title}</h1>
+                            <p class="paragraph">${blocksText.body}</div>
+                        </div>`;
+
+        html += htmlText;
+
+        container.innerHTML = html
+      
+    });
+  };
+
+  renderListItems();
+
+
+
+
